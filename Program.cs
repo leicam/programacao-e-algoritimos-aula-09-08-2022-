2162485ba0158a88ffde6b5390b7d0c50f588283
Juliano Ribeiro de Souza Maciel <juliano.maciel@faculdadefacec.edu.br>
2022-08-09 20:13:36 -0300

    Implementações com File

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace MeuSegundoProjeto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var dados = @"L:\LAB01\ALgoritimos e Programacao\" +
+                @"MeuSegundoProjeto\MeuSegundoProjeto\dados.txt";
+
+            Console.WriteLine("Carregando dados. Aguarde...");
+            var texto = File.ReadAllText(dados);
+
+            Console.WriteLine(texto);
+            Console.WriteLine("Pressione alguma tecla para continuar...");
+            Console.ReadKey();
+        }
+    }
+}
